0a37f8c8dcbfce9de33e9fab95756f49123815c5
Jon Wheeler <wheelibin@gmail.com>
2018-10-08 14:15:29 +0100

    Added proptypes to Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Snackbar } from "@material-ui/core";
 
 const Alert = ({ hideAfter, open, onClose, content }) => {
   return (
     <Snackbar autoHideDuration={hideAfter} anchorOrigin={{ vertical: "top", horizontal: "right" }} open={open} onClose={onClose} message={content} />
   );
+};
+
+Alert.propTypes = {
+  hideAfter: PropTypes.number.isRequired,
+  open: PropTypes.bool,
+  onClose: PropTypes.func.isRequired,
+  content: PropTypes.string.isRequired
 };
 
 export default Alert;
