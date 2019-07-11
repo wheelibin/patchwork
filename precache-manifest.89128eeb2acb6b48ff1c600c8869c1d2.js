5854c13f80b0a6841f4d7855d12f5bbf2dcffa74
Jon Wheeler <wheelibin@gmail.com>
2019-07-11 20:53:15 +0100

    Updates

---
+++ precache-manifest.89128eeb2acb6b48ff1c600c8869c1d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af152a29e33852d85d2c9c5a11ee4bcf",
+    "url": "/patchwork/index.html"
+  },
+  {
+    "revision": "b7f488cd878ef2098b77",
+    "url": "/patchwork/static/css/2.e21c74bf.chunk.css"
+  },
+  {
+    "revision": "7595ff25697cc89705d1",
+    "url": "/patchwork/static/css/main.22de8ada.chunk.css"
+  },
+  {
+    "revision": "b7f488cd878ef2098b77",
+    "url": "/patchwork/static/js/2.7e3ccb6d.chunk.js"
+  },
+  {
+    "revision": "7595ff25697cc89705d1",
+    "url": "/patchwork/static/js/main.90a6452e.chunk.js"
+  },
+  {
+    "revision": "3e8adf15179fc87c5fa0",
+    "url": "/patchwork/static/js/runtime~main.c5928f42.js"
+  }
+]);
